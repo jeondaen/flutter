817a3d3ff701235e5dd99d13d042df9be0ce6af6
jeondaen <jeondaen@gmail.com>
2020-07-24 15:37:59 +0900

    ex8 DONE

--- ex8/lib/main.dart
+++ ex8/lib/main.dart
@@ -1,40 +1,83 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-main(List<String> args) {
-  runApp(MyApp());
+void main() {
+  runApp(SuperApp());
 }
 
-class MyApp extends StatelessWidget {
+class SuperApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Ex8",
+      title: "EX8",
       home: HomeApp(),
     );
   }
 }
 
-// class HomeApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text("Sign in"),
-//       ),
-//       body: IndexApp(),
-//     );
-//   }
-// }
+class HomeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("EX8"),
+      ),
+      body: BodyApp(),
+    );
+  }
+}
 
-// class IndexApp extends StatefulWidget {
-//   @override
-//   _IndexAppState createState() => _IndexAppState();
-// }
+class BodyApp extends StatefulWidget {
+  @override
+  _BodyAppState createState() => _BodyAppState();
+}
 
-// class _IndexAppState extends State<IndexApp> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container();
-//   }
-//}
+class _BodyAppState extends State<BodyApp> {
+  String username = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(200, 100, 200, 0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextField(
+            onChanged: (value) => username = value,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Username',
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 20.0),
+          ),
+          TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Password',
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 20.0),
+          ),
+          RaisedButton(
+            onPressed: () {
+              final snackBar = SnackBar(
+                content: Text(username),
+                action: SnackBarAction(
+                  label: 'Undo',
+                  onPressed: () {},
+                ),
+              );
+
+              Scaffold.of(context).showSnackBar(snackBar);
+            },
+            child: Text("Sign in"),
+          )
+        ],
+      ),
+    );
+  }
+}
